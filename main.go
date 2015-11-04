ab2ad7eff42fada12f6b61d289ad4846f6401db3
Håkon Antonsen <haakon@antons1.net>
2015-11-04 21:24:39 +0100

    Moved output to Questionnaire.go, and added it

--- main.go
+++ main.go
@@ -5,5 +5,15 @@ import(
 )
 
 func main() {
+	dil := Dilemma{};
+	dil.createDilemmaFromFile("./pc.json");
 	
+	if dil.Title == "" {
+		fmt.Printf("There was an error reading the file. See above for more info\n");
+		return;
+	}
+	
+	sc := Score{};
+	sc.initScore(dil);
+	sc.FindScore();
 }
