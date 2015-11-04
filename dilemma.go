16963920933205e5076842fea5b577de99dbd435
Håkon Antonsen <haakon@antons1.net>
2015-11-04 20:48:48 +0100

    Loading dilemma json now works. Also renamed Requirements.go to Dilemma.go

---
+++ dilemma.go
@@ -0,0 +1,49 @@
+package main
+
+import(
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+)
+
+/**
+Structs to store a dilemma, with requirements and alternatives.
+*/
+type Alternative struct {
+	Name string
+	Weight float64
+}
+
+type Requirement struct {
+	Name string
+	Description string
+	Importance float64
+	Alternatives []interface{}
+}
+
+type Dilemma struct {
+	Title string
+	Description string
+	Requirements []interface{}
+}
+
+// Reads a dilemma from the given file.
+func (dil *Dilemma) createDilemmaFromFile(file string) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Print("Error not null!\n")
+		fmt.Printf("%s\n", err)
+		fmt.Println()
+		dil.Title = "";
+		return
+	}
+	
+	err = json.Unmarshal(data, dil)	
+	if err != nil {
+		fmt.Print("Error not null!\n")
+		fmt.Printf("%s\n", err)
+		fmt.Println()
+		dil.Title = "";
+		return
+	}
+}
